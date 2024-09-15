866ec84173747f1c30f937e7344dfc07b98f41fb
Paulo Bandua <pcbandua@gmail.com>
2024-09-15 15:50:28 -0700

    first commit

---
+++ practice1.js
@@ -0,0 +1,22 @@
+// Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
+
+// var first = "Banksy"
+// var last = "Fancy"
+
+// console.log(first + " " + last)
+
+
+// Write a program that asks the user to enter a password. If the password is "Joshua", the program responds "Shall we play a game?". For any other password, the program responds "Access denied"
+
+var input = window.prompt("Enter a password: ")
+if (input === "Joshua") {
+  console.log("Shall we play a game?");
+}
+else {
+  console.log("Access denied");
+}
+
+// Start with an array of strings and create a new array with each string upcased.
+// For example, ["hello", "goodbye"] becomes ["HELLO", "GOODBYE"].
+
+
